414296211bbe2e4dc377494b13d61565b8ed455a
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-08-22 09:05:24 -0400

    Move xaringan settings to shared R source file and update formatting   * The knitr::opts_chunk$set() code that starts every setup code block has been     moved to a single source file under src/xaringan_setup.R. If different     settings are needed, they can be overridden after the file is sourced.   * Move the dev: svg setting into the _output.yaml file   * Add echo = FALSE to all knitr::include_graphics() blocks in slides for     classes 2, 4, and 5 now that the default via src/xaringan_setup.R is     echo = TRUE and eval = TRUE   * Remove quietly = TRUE from library() commands as     suppressPackageStartupMessages already handles that

---
+++ src/xaringan_setup.R
@@ -0,0 +1,13 @@
+knitr::opts_chunk$set(
+  echo = TRUE,
+  eval = TRUE,
+  fig.width = 6,
+  warning = FALSE,
+  message = FALSE,
+  fig.asp = 0.618,
+  out.width = "70%",
+  dpi = 120,
+  fig.align = "center",
+  cache = FALSE
+)
+options(htmltools.dir.version = FALSE)
